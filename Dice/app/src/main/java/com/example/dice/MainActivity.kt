ff3fcff5b9f79e79733c8fde9e33ceb8d4ae337e
Heeeon <51069854+bbjoite09@users.noreply.github.com>
2021-07-20 23:01:35 +0900

    style : MODIFY code style

--- Dice/app/src/main/java/com/example/dice/MainActivity.kt
+++ Dice/app/src/main/java/com/example/dice/MainActivity.kt
@@ -19,27 +19,35 @@ class MainActivity : AppCompatActivity() {
             // 버튼 누를때마다 textView 랜덤하게 변경
             rollDice()
         }
+        rollDice()
     }
 
     private fun rollDice() {
         // 6면의 주사위를 랜덤하게 던짐
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        
+
 //        // 랜덤한 숫자를 textView에 넣기
 //        val resultTextView: TextView = findViewById(R.id.textView)
 //        resultTextView.text = diceRoll.toString()
 
+        // ImageView id 얻기
         val diceImage: ImageView = findViewById(R.id.imageView)
 
-        when(diceRoll){
-            1 -> diceImage.setImageResource(R.drawable.dice_1)
-            2 -> diceImage.setImageResource(R.drawable.dice_2)
-            3 -> diceImage.setImageResource(R.drawable.dice_3)
-            4 -> diceImage.setImageResource(R.drawable.dice_4)
-            5 -> diceImage.setImageResource(R.drawable.dice_5)
-            6 -> diceImage.setImageResource(R.drawable.dice_6)
+        // 랜덤하게 결정된 diceRoll의 값에 따라 이미지 아이디를 저장
+        val drawableResource = when(diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
+
+        // 이미지 업데이트
+        diceImage.setImageResource(drawableResource)
+        // content description 업데이트
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
 
