f06941271111f2a8ca4b7a3f2e685bfd61cd134c
Heeeon <51069854+bbjoite09@users.noreply.github.com>
2021-07-21 01:31:22 +0900

    feat : 상속

--- Calculator/app/src/main/java/com/example/calculator/learnClass.kt
+++ Calculator/app/src/main/java/com/example/calculator/learnClass.kt
@@ -8,6 +8,22 @@ fun main() {
         println("Material: ${buildingMaterial}")
         println("Has room? ${hasRoom()}")
     }
+
+    val roundHut = RoundHut(3)
+    with(roundHut) {
+        println("\nRound Hut\n=========")
+        println("Material: ${buildingMaterial}")
+        println("Capacity: ${capacity}")
+        println("Has room? ${hasRoom()}")
+    }
+
+    val roundTower = RoundTower(4, 3)
+    with(roundTower) {
+        println("\nRound Tower\n==========")
+        println("Material: ${buildingMaterial}")
+        println("Capacity: ${capacity}")
+        println("Has room? ${hasRoom()}")
+    }
 }
 
 abstract class Dwelling(private var residents: Int) {
@@ -22,5 +38,14 @@ abstract class Dwelling(private var residents: Int) {
 class SquareCabin(residents: Int) : Dwelling(residents) {
     override val buildingMaterial = "Wood"
     override val capacity = 6
+}
 
+open class RoundHut(residents: Int) : Dwelling(residents){
+    override val buildingMaterial = "Straw"
+    override val capacity = 4
+}
+
+class RoundTower(residents: Int, floors:Int): RoundHut(residents){
+    override val buildingMaterial = "Stone"
+    override val capacity = 4 * floors
 }
