f51d4867592cdb73ab352bfcd5b0f3dc7f84f881
Heeeon <51069854+bbjoite09@users.noreply.github.com>
2021-07-21 01:16:23 +0900

    feat : 추상클래스, 상속

---
+++ Calculator/app/src/main/java/com/example/calculator/learnClass.kt
@@ -0,0 +1,21 @@
+package com.example.calculator
+
+fun main() {
+    val squareCabin = SquareCabin(6)
+
+}
+
+abstract class Dwelling(private var residents: Int) {
+    abstract val buildingMaterial: String
+    abstract val capacity: Int
+
+    fun hasRoom(): Boolean {
+        return residents < capacity
+    }
+}
+
+class SquareCabin(residents: Int) : Dwelling(residents) {
+    override val buildingMaterial = "Wood"
+    override val capacity = 6
+
+}
