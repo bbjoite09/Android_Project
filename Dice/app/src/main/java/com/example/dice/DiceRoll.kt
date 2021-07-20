148129a3e62a59e7283d2567fd4fc2f92dcf3593
Heeeon <51069854+bbjoite09@users.noreply.github.com>
2021-07-20 22:28:21 +0900

    feat : when

--- Dice/app/src/main/java/com/example/dice/DiceRoll.kt
+++ Dice/app/src/main/java/com/example/dice/DiceRoll.kt
@@ -8,18 +8,13 @@ fun main() {
 
     val luckyNumber = 4
 
-    if (rollResult == luckyNumber) {
-        println("You win!")
-    } else if (rollResult == 1) {
-        println("So sorry! You rolled a 1. Try again!")
-    } else if (rollResult == 2) {
-        println("Sadly, you rolled a 2. Try again!")
-    } else if (rollResult == 3) {
-        println("Unfortunately, you rolled a 3. Try again!")
-    } else if (rollResult == 5) {
-        println("Don't cry! You rolled a 5. Try again!")
-    } else {
-        println("Apologies! You rolled a 6. Try again!")
+    when(rollResult){
+        luckyNumber -> println("You win!")
+        1 -> println("So sorry! You rolled a 1. Try again!")
+        2 -> println("Sadly, you rolled a 2. Try again!")
+        3 -> println("Unfortunately, you rolled a 3. Try again!")
+        5 -> println("Don't cry! You rolled a 5. Try again!")
+        else -> println("Apologies! you rolled a 6. Try again!")
     }
 }
 
